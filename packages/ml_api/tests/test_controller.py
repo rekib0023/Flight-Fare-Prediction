f61a7c00865ccdf79322ed24b4bfb720073ce1d8
rekib <rkb.ra0025@gmail.com>
2020-08-13 00:07:30 +0530

    prediction endpoint

--- packages/ml_api/tests/test_controller.py
+++ packages/ml_api/tests/test_controller.py
@@ -5,6 +5,13 @@ Created on Wed Aug 12 23:43:23 2020
 @author: rkbra
 """
 
+from regression_model.config import config as model_config
+from regression_model.processing.data_management import load_dataset
+from regression_model import __version__ as _version
+
+import json
+import math
+
 
 def test_health_endpoint_returns_200(flask_test_client):
     # When
@@ -12,3 +19,25 @@ def test_health_endpoint_returns_200(flask_test_client):
 
     # Then
     assert response.status_code == 200
+
+
+def test_prediction_endpoint_returns_prediction(flask_test_client):
+    # Given
+    # Load the test data from the regression_model package
+    # This is important as it makes it harder for the test
+    # data versions to get confused by not spreading it
+    # across packages.
+    test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
+    post_json = test_data[0:1].to_json(orient='records')
+
+    # When
+    response = flask_test_client.post('/v1/predict/regression',
+                                      json=post_json)
+
+    # Then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    prediction = response_json['predictions']
+    response_version = response_json['version']
+    assert math.ceil(prediction) == 9956
+    assert response_version == _version
