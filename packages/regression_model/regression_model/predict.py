07c5c00e8f7361ac08b5699637d3e8157e096f20
rekib <rkb.ra0025@gmail.com>
2020-08-15 21:36:29 +0530

    misc

--- packages/regression_model/regression_model/predict.py
+++ packages/regression_model/regression_model/predict.py
@@ -13,7 +13,7 @@ from regression_model.config import config
 from regression_model import __version__ as _version
 
 import logging
-
+import typing as t
 
 _logger = logging.getLogger(__name__)
 
@@ -22,8 +22,16 @@ pipeline_file_name = f"{config.PIPELINE_SAVE_FILE}{_version}.pkl"
 pipeline = load_pipeline(file_name=pipeline_file_name)
 
 
-def make_prediction(*, input_data) -> dict:
-    """Make a prediction using the saved model pipeline."""
+def make_prediction(*, input_data: t.Union[pd.DataFrame, dict],
+                    ) -> dict:
+    """Make a prediction using a saved model pipeline.
+
+    Args:
+        input_data: Array of model prediction inputs.
+
+    Returns:
+        Predictions for each input row, as well as the model version.
+    """
     
     data = pd.DataFrame(input_data)
     prediction = pipeline.predict(data[config.FEATURES])
--- packages/regression_model/tests/test_predict.py
+++ packages/regression_model/tests/test_predict.py
@@ -14,11 +14,12 @@ from regression_model.processing.data_management import load_dataset
 def test_make_single_prediction():
     # GIven
     test_data = load_dataset(file_name='test.xlsx')
-    single_test_json = test_data[0:1]
-    
-    subject = make_prediction(input_data=single_test_json)
-    
-    
+    single_test_input = test_data[0:1]
+
+    # When
+    subject = make_prediction(input_data=single_test_input)
+
+    # Then
     assert subject is not None
     assert isinstance(subject.get('predictions')[0], float)
     
@@ -27,10 +28,10 @@ def test_make_multiple_predictions():
     # Given
     test_data = load_dataset(file_name='test.xlsx')
     original_data_length = len(test_data)
-    multiple_test_json = test_data
+    multiple_test_input = test_data
 
     # When
-    subject = make_prediction(input_data=multiple_test_json)
+    subject = make_prediction(input_data=multiple_test_input)
 
     # Then
     assert subject is not None
